9ad9791be731a383bd7f36fa22b23f48bc86c8e3
Krzysztof Nowak <992haslo992@gmail.com>
2025-05-26 21:47:12 +0200

    working response with jwt token

--- src/components/NotHome.jsx
+++ src/components/NotHome.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react"
+import useAuth from "../hooks/useAuth"
+import axios from "../api/axios";
 
 const NotHome = () => {
-
+    const {auth} = useAuth()
+const fetchUsers = async () => {
+        try {
+            const response = await axios.get('./allusers', {
+                headers: {
+                    'Authorization': `Bearer ${auth.token}` // 'Authorization', not 'Authentication'
+                },
+                withCredentials: true
+            });
+            console.log(response.data); // Do something with the data
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
+    };
+    fetchUsers()
     return(
         <div>
             <h1>menager only</h1>
